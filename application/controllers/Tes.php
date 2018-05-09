48e894a2f11c74eb0721032d7b757dd6cebad857
lanuma <lanuma.angga@gmail.com>
2018-05-09 16:17:48 +0700

    Commit pertama

---
+++ application/controllers/Tes.php
@@ -0,0 +1,13 @@
+<?php if(!defined('BASEPATH')) exit('ZZzzzzzz');
+
+Class Tes extends CI_Controller {
+	function __construct()
+	{
+		parent::__construct();
+	}
+	
+	function index()
+	{
+		echo "tes";
+	}
+}
